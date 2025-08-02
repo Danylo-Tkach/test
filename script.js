f777ba73242035c4ef75b442d4ab973feb20532e
Danylo <danilotkac3@gmail.com>
2025-08-02 09:46:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const hours = now.getHours();
 const daysUa = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П’ятниця', 'Субота'];
 const dayName = daysUa[dayOfWeek];
 
-if (dayOfWeek !== 6 && dayOfWeek !== 0) {
+if (dayOfWeek == 1) { // Понеділок
     if (hours >= 9 && hours < 13) {
         text.style.color = 'green';
         text.textContent = 'Відчинено';
@@ -25,15 +25,115 @@ if (dayOfWeek !== 6 && dayOfWeek !== 0) {
         text.style.color = 'red';
         text.textContent = 'Зачинено';
     }
-} else if (dayOfWeek === 0) {
-    if (hours >= 9 && hours < 15) {
+}
+if (dayOfWeek == 2) { // Вівторок
+    if (hours >= 9 && hours < 13) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else if (hours >= 13 && hours < 14) {
+        text.style.color = 'gold';
+        text.textContent = 'Обід';
+    } else if (hours >= 14 && hours < 18) {
         text.style.color = 'green';
         text.textContent = 'Відчинено';
     } else {
         text.style.color = 'red';
         text.textContent = 'Зачинено';
     }
-} else {
+}
+if (dayOfWeek == 3) { // Середа
+    if (hours >= 9 && hours < 13) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else if (hours >= 13 && hours < 14) {
+        text.style.color = 'gold';
+        text.textContent = 'Обід';
+    } else if (hours >= 14 && hours < 18) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else {
+        text.style.color = 'red';
+        text.textContent = 'Зачинено';
+    }
+}
+if (dayOfWeek == 4) { // Четвер
+    if (hours >= 9 && hours < 13) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else if (hours >= 13 && hours < 14) {
+        text.style.color = 'gold';
+        text.textContent = 'Обід';
+    } else if (hours >= 14 && hours < 18) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else {
+        text.style.color = 'red';
+        text.textContent = 'Зачинено';
+    }
+}
+if (dayOfWeek == 5) { // П'ятниця
+    if (hours >= 9 && hours < 13) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else if (hours >= 13 && hours < 14) {
+        text.style.color = 'gold';
+        text.textContent = 'Обід';
+    } else if (hours >= 14 && hours < 18) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else {
+        text.style.color = 'red';
+        text.textContent = 'Зачинено';
+    }
+}
+if (dayOfWeek == 6) { // Субота
     text.style.color = 'red';
     text.textContent = 'Зачинено';
 }
+if (dayOfWeek == 0) { // Неділя
+    if (hours >= 9 && hours < 13) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else if (hours >= 13 && hours < 14) {
+        text.style.color = 'gold';
+        text.textContent = 'Обід';
+    } else if (hours >= 14 && hours < 18) {
+        text.style.color = 'green';
+        text.textContent = 'Відчинено';
+    } else {
+        text.style.color = 'red';
+        text.textContent = 'Зачинено';
+    }
+}
+
+
+
+
+
+
+// if (dayOfWeek !== 6 && dayOfWeek !== 0) {
+//     if (hours >= 9 && hours < 13) {
+//         text.style.color = 'green';
+//         text.textContent = 'Відчинено';
+//     } else if (hours >= 13 && hours < 14) {
+//         text.style.color = 'gold';
+//         text.textContent = 'Обід';
+//     } else if (hours >= 14 && hours < 18) {
+//         text.style.color = 'green';
+//         text.textContent = 'Відчинено';
+//     } else {
+//         text.style.color = 'red';
+//         text.textContent = 'Зачинено';
+//     }
+// } else if (dayOfWeek === 0) {
+//     if (hours >= 9 && hours < 15) {
+//         text.style.color = 'green';
+//         text.textContent = 'Відчинено';
+//     } else {
+//         text.style.color = 'red';
+//         text.textContent = 'Зачинено';
+//     }
+// } else {
+//     text.style.color = 'red';
+//     text.textContent = 'Зачинено';
+// }
